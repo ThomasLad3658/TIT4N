2efd6921d05dcfe167cccd3f16dab2f47a5894f8
M3DAY00 <144501112+M3DAY00@users.noreply.github.com>
2026-06-08 12:18:27 -0400

    Added music demo

--- Game/main.lua
+++ Game/main.lua
@@ -1,9 +1,15 @@
+gravity = 30
+
 CreateWindow("TIT4N",  1920, 1080)
 SetFrameRate(60)
 SetWindowFullscreen(true)
 LoadScene("menus/menu1")
-
-gravity = 30
+forest = CreateSound("assets/sounds/forest.wav")
+PlaySound(forest)
+LoopSound(forest, true)
+music = CreateSound("assets/sounds/music.wav")
+PlaySound(music)
+LoopSound(music, true)
 
 function OnUpdate(dt)
 	if GetKeyState("Escape") == 2 then
